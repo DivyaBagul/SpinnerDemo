3159206f2020eea7e3217e8af1d9023f84275036
Divya Bagul <divya.bagul2530@gmail.com>
2019-05-31 14:04:37 +0530

    Initial commit

---
+++ app/src/main/java/com/example/spinnerdemo/MainActivity.java
@@ -0,0 +1,47 @@
+package com.example.spinnerdemo;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity implements AdapterView.OnItemSelectedListener {
+    Spinner spinner;
+    TextView textView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+         spinner = findViewById(R.id.planets_spinner);
+         textView=findViewById(R.id.text);
+// Create an ArrayAdapter using the string array and a default spinner layout
+
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this,
+                R.array.planets_array, android.R.layout.simple_spinner_item);
+// Specify the layout to use when the list of choices appears
+
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+// Apply the adapter to the spinner
+        spinner.setAdapter(adapter);
+        spinner.setOnItemSelectedListener(this);
+
+    }
+
+    @Override
+    public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+        //String item = parent.getItemAtPosition(position).toString();
+        Toast.makeText(parent.getContext(),spinner.getSelectedItem().toString(), Toast.LENGTH_LONG).show();
+        textView.setText(spinner.getSelectedItem().toString());
+
+    }
+
+    @Override
+    public void onNothingSelected(AdapterView<?> parent) {
+
+    }
+}
